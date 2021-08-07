8444d5e7148945965be7cb0786bd1972f8ad99dd
arcpac <antonraphaelcaballes@gmail.com>
2021-08-07 18:26:34 +0800

    Reduce

--- pie/functional.py
+++ pie/functional.py
@@ -1,8 +1,15 @@
+# Zip map, reduce and filter = FUNCTIONAL
 my_list = [1,2,3,4]
+your_list = (10,11,12)
+their_list = (6,7,1)
+
 def mult_by2(item):
     return item*2
 
 def check_odd(item):
     return item % 2 != 0
-        
+
+print(list(map(mult_by2, my_list)))
 print(list(filter(check_odd, my_list)))
+print(list(zip(my_list, your_list)))
+print(list(zip(my_list, your_list, their_list)))
